8fcb2a9a5484265e01b1366d06fadbe31da75136
Wastabir Rajvee Alam <wastabirrajveealamsa_cse10@its.edu.in>
2018-09-29 21:58:28 -0400

    Small change

--- BestFirstSearch.java
+++ BestFirstSearch.java
@@ -1,9 +1,10 @@
+import java.util.PriorityQueue;
 
 public class BestFirstSearch {
 	
 	private Board board;
 	private Node goalNode = null;
-	
+	PriorityQueue<Node> queue=new PriorityQueue<>(new NodeComparator());
 	
 	public BestFirstSearch(Board board) {
 		this.board = board;
